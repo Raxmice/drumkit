2edb35f29b1280a3fc8368f3856eeafbdc784ec8
Raxmice <93811296+Raxmice@users.noreply.github.com>
2022-03-29 12:12:48 +0530

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,35 @@
+function w(){
+    var audio = new Audio('https://github.com/Piyush-Chetwani/Drum-Kit/blob/main/sounds/tom-1.mp3?raw=true');
+    audio.currentTime=0;
+    audio.play();
+}
+function a(){
+    var audio = new Audio("https://github.com/Piyush-Chetwani/Drum-Kit/blob/main/sounds/tom-2.mp3?raw=true"); 
+    audio.currentTime=0;
+    audio.play();
+}
+function s(){
+    var audio = new Audio("https://github.com/Piyush-Chetwani/Drum-Kit/blob/main/sounds/tom-3.mp3?raw=true"); 
+    audio.currentTime=0;
+    audio.play();
+}
+function d(){
+    var audio = new Audio("https://github.com/Piyush-Chetwani/Drum-Kit/blob/main/sounds/tom-4.mp3?raw=true"); 
+    audio.currentTime=0;
+    audio.play();
+}
+function j(){
+    var audio = new Audio("https://github.com/Piyush-Chetwani/Drum-Kit/blob/main/sounds/snare.mp3?raw=true"); 
+    audio.currentTime=0;
+    audio.play();
+}
+function k(){
+    var audio = new Audio("https://github.com/Piyush-Chetwani/Drum-Kit/blob/main/sounds/crash.mp3?raw=true"); 
+    audio.currentTime=0;
+    audio.play();
+}
+function l(){
+    var audio = new Audio("https://github.com/Piyush-Chetwani/Drum-Kit/blob/main/sounds/kick-bass.mp3?raw=true"); 
+    audio.currentTime=0;
+    audio.play();
+}
